614765aeac8b8223fd20e57430a552bbf0b7b0bc
andSpt <henrrry52@gmail.com>
2025-03-06 19:37:22 +0300

    Fix locators.py

--- locators.py
+++ locators.py
@@ -1,34 +1,42 @@
 from selenium.webdriver.common.by import By
 
 
-class SBISLocators():
-    SBIS_CONTASTS: tuple = (By.CSS_SELECTOR, ".sbisru-Header [href='/contacts']")
+class MainPageLocators:
+    """Locators main page"""
+
+    CONTACTS: tuple[str, str] = (By.CSS_SELECTOR, "ul.sbisru-Header__menu div.sbisru-Header__menu-link")
+    CONTACTS_MORE: tuple[str, str] = (By.CSS_SELECTOR, "ul.sbisru-Header__menu a.sbisru-link.sbis_ru-link span")
+    DOWNLOAD_LOCAL_VERSIONS: tuple[str, str] = (By.XPATH, "//a[text()='Скачать локальные версии']")
 
 
-class SBISContactsLocators():
-    BANNER_TENSOR: tuple = (By.CSS_SELECTOR, "#contacts_clients a[href='https://tensor.ru/']")
-    MY_REGION: tuple = (By.CSS_SELECTOR,
-                        "#container > div.sbis_ru-content_wrapper.ws-flexbox.ws-flex-column > div > div.sbis_ru-container.sbisru-Contacts__relative > div.s-Grid-container.s-Grid-container--space.s-Grid-container--alignEnd.s-Grid-container--noGutter.sbisru-Contacts__underline > div:nth-child(1) > div > div:nth-child(2) > span > span")
-    LIST_PARTNERS: tuple = (
-    By.CSS_SELECTOR, "#contacts_list > div > div.ws-flexbox.sbisru-Contacts-City__flex.sbisru-Contacts-Shot")
-    REGION_KAMCHAT_KRAI: tuple = (By.CSS_SELECTOR,
-                                  "#popup > div.controls-Popup.ws-float-area-show-complete.controls-Popup_shown.controls_themes__wrapper.controls-Scroll_webkitOverflowScrollingTouch.controls-Popup__lastItem > div > div > div > div > div.sbis_ru-Region-Panel.sbis_ru-Region-Panel-l > div > ul > li:nth-child(43) > span > span")
+class SbisContactsLocators:
+    """Locators contacts page"""
+
+    BANNER_TENSOR: tuple[str, str] = (By.CSS_SELECTOR, "div#contacts_clients div.sbisru-Contacts__border-left img")
+    REGION: tuple[str, str] = (By.CSS_SELECTOR, "div.sbis_ru-container.sbisru-Contacts__relative span span")
+    LIST_PARTNERS: tuple[str, str] = (By.CSS_SELECTOR, "div#contacts_list div.sbisru-Contacts-List__col div[name='itemsContainer'] div#city-id-2")
+    REGION_KAMCHATKA: tuple[str, str] = (By.XPATH, "//span[text()='41 Камчатский край']")
 
 
-class TensorLocators():
-    STRENGHT_IS_IN_PEOPLE_TEXT: tuple = (By.CSS_SELECTOR,
-                                         "#container > div.tensor_ru-content_wrapper > div > div.tensor_ru-Index__block4-bg > div > div > div:nth-child(1) > div > p.tensor_ru-Index__card-title.tensor_ru-pb-16")
-    STRENGHT_IS_IN_PEOPLE_MORE_DETAILS: tuple = (By.CSS_SELECTOR,
-                                                 "#container > div.tensor_ru-content_wrapper > div > div.tensor_ru-Index__block4-bg > div > div > div:nth-child(1) > div > p:nth-child(4) > a")
+class TensorLocators:
+    """Locators tensor page"""
+
+    BLOCK_STRENGTH_IN_PEOPLE: tuple[str, str] = (By.CSS_SELECTOR, "div.tensor_ru-Index__block4-content p:nth-of-type(1)")
+    MORE_DETAILS: tuple[str, str] = (By.CSS_SELECTOR, "div.tensor_ru-Index__block4-bg p:nth-child(4) a")
 
 
-class TensorMoreDetailsLocators():
-    IMAGE_DEVELOP_SBIS: tuple = (By.CSS_SELECTOR,
-                                 "#container > div.tensor_ru-content_wrapper > div > div.tensor_ru-container.tensor_ru-section.tensor_ru-About__block3 > div.s-Grid-container > div:nth-child(1) > a > div.tensor_ru-About__block3-image-wrapper > img")
+class AboutLocators:
+    """Locators about page"""
 
-    IMAGE_CREATE_INFRASTRUCTURE: tuple = (By.CSS_SELECTOR,
-                                          "#container > div.tensor_ru-content_wrapper > div > div.tensor_ru-container.tensor_ru-section.tensor_ru-About__block3 > div.s-Grid-container > div:nth-child(3) > a > div.tensor_ru-About__block3-image-wrapper > img")
-    IMAGE_ACCOMPANYING_CLIENT: tuple = (By.CSS_SELECTOR,
-                                        "#container > div.tensor_ru-content_wrapper > div > div.tensor_ru-container.tensor_ru-section.tensor_ru-About__block3 > div.s-Grid-container > div:nth-child(4) > a > div.tensor_ru-About__block3-image-wrapper > img")
-    IMAGE_PROMOTE_SERVICES: tuple = (By.CSS_SELECTOR,
-                                     "#container > div.tensor_ru-content_wrapper > div > div.tensor_ru-container.tensor_ru-section.tensor_ru-About__block3 > div.s-Grid-container > div:nth-child(2) > a > div.tensor_ru-About__block3-image-wrapper > img")
+    IMG_DEV_SISTEM_SABY: tuple[str, str] = (By.CSS_SELECTOR, "div.tensor_ru-container.tensor_ru-About__block3 div.s-Grid-container div.s-Grid-col:nth-of-type(1) img")
+    IMG_PROMOTE_SERVICES: tuple[str, str] = (By.CSS_SELECTOR, "div.tensor_ru-container.tensor_ru-About__block3 div.s-Grid-container div.s-Grid-col:nth-of-type(2) img")
+    IMG_CREATE_INFRASTRUCTURE: tuple[str, str] = (By.CSS_SELECTOR, "div.tensor_ru-container.tensor_ru-About__block3 div.s-Grid-container div.s-Grid-col:nth-of-type(3) img")
+    IMG_ASSIST_CLIENTS: tuple[str, str] = (By.CSS_SELECTOR, "div.tensor_ru-container.tensor_ru-About__block3 div.s-Grid-container div.s-Grid-col:nth-of-type(4) img")
+
+
+class DownloadSabyLocators:
+    """Locators download Saby page"""
+
+    SBIS_PLUGIN: tuple[str, str] = (By.XPATH, "//div[text()='Saby Plugin']")
+    TAB_WINDOWS: tuple[str, str] = (By.XPATH, "//span[text()='Windows']")
+    FILE_WINDOWS_DOWNLOAD_EXE: tuple[str, str] = (By.XPATH, "//a[contains(text(), 'Скачать (Exe')]")
