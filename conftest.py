458b1e2926af103bfe4eac18b16c9cd955e24062
andSpt <henrrry52@gmail.com>
2025-03-06 19:36:28 +0300

    Fix conftest.py. Add default dir download

--- conftest.py
+++ conftest.py
@@ -1,36 +1,26 @@
+from pathlib import Path
 import pytest
+import shutil
+
+from selenium.webdriver.remote.webdriver import WebDriver
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options as OptionsFirefox
 
 
-def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default='chrome',
-                     help="Choose browser: '--browser_name=chrome' or '--browser_name=firefox'")
+DOWNLOAD_DIR: Path = Path.cwd() / 'download/'
 
-    parser.addoption('--language', action='store', default='None',
-                     help="Choose language: '--language=en' or '--language=ru'")
-
-
-@pytest.fixture(scope='function')
-def browser(request, user_language=None):
-    browser_name = request.config.getoption("browser_name")
-    user_language = request.config.getoption("language")
-
-    options = Options()
-    options.add_experimental_option("prefs", {"intl.accept_languages": user_language})
-    options.add_argument("--start-maximized")
-    options_firefox = OptionsFirefox()
-    options_firefox.set_preference("intl.accept_languages", user_language)
-
-    if browser_name == "chrome":
-        browser = webdriver.Chrome(options=options)
-
-    elif browser_name == "firefox":
-        browser = webdriver.Firefox(options=options)
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
-
+@pytest.fixture(scope='module')
+def browser():
+    chrome_options: Options = Options()
+    chrome_options.add_experimental_option(
+        "prefs", {
+        "download.default_directory": str(DOWNLOAD_DIR),
+        "download.prompt_for_download": False,
+        "safebrowsing.allow_downloads_from_hosts": "https://sbis.ru/",
+        "safebrowsing.enabled": True,
+        "download.directory_upgrade": True,
+    })
+    browser: WebDriver = webdriver.Chrome(options=chrome_options)
     yield browser
     browser.quit()
 
